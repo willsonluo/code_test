020a7e44cb86ea7773fb83349bf16d5b7070dfe0
willsonluo <Willson_luo@163.com>
2021-11-25 10:27:49 +0800

    update.

--- test1.py
+++ test1.py
@@ -6,17 +6,23 @@ def solution(A):
         # print(B)
         result = []
         for i in range(len(B)-1):
-            if B[i] + 1 == B[i+1]:
+            if B[i] + 2 == B[i+1]:
                 # print("item:",B[i],B[i+1])
                 idx_a = A.index(B[i])
                 idx_b = A.index(B[i+1])
                 # print("index:",idx_a,idx_b)
-                result.append(abs(idx_b-idx_a))
-        print(max(result))
-        return max(result)      
+                result.append(abs(idx_b-idx_a))                
+        if len(result) != 0:      
+            print(max(result))
+            return max(result)     
+        else:
+            print(-1)
+            return -1 
     else:
+        print(-1)
         return -1
 
-
+solution([0,0,0,0,0,0,0,0,1,2])
+solution([0,0,0,0,0,0,0,0])
 solution([0,3,3,7,5,3,11,1])
 solution([1,4,7,3,3,5])
--- test2.py
+++ test2.py
@@ -15,12 +15,14 @@ def solution(A):
                 B.append(A[i])
                 flag = 2
         # print(B)
-        # print(max(max_flag,flag))
+        print(max(max_flag,flag))
         return max(max_flag,flag)
     else: 
+        print(0)
         return 0
     
 solution([4,2,2,4,2])
 solution([1,2,3,2])
 solution([0,5,4,4,5,12])
 solution([4,4])
+solution([0,0,0,9,8,0,0,0])
